a24a1c1a5f7e37be48bf3467abe50039c681e241
Ken Kimura <ken.kimura1988@outlook.com>
2019-07-10 23:44:55 -0400

    Add Another Ref Exist Handler

--- src/ArcherContainer.js
+++ src/ArcherContainer.js
@@ -199,7 +199,9 @@ export class ArcherContainer extends React.Component<Props, State> {
 
   unregisterChild = (id: string): void => {
     const { refs, observer } = this.state;
-    observer.unobserve(refs[id]);
+    if (refs[id]) {
+      observer.unobserve(refs[id]);
+    }
     const newRefs = { ...refs };
     delete newRefs[id];
     this.setState(() => ({ refs: newRefs }));
